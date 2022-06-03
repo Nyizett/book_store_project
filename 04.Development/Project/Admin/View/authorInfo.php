c2f632ed8220752f3497a07c41e6f9bcf71b476d
Merries <merriesmmk5814@gmail.com>
2022-06-03 19:49:31 +0630

    authorListController

--- 04.Development/Project/Admin/View/authorInfo.php
+++ 04.Development/Project/Admin/View/authorInfo.php
@@ -14,7 +14,7 @@
     <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
     <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
     <script src="../resource/js/common.js"></script>
-    
+
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </head>
@@ -22,35 +22,34 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-        <div id="navbar" class="my-nav"></div>
+            <div id="navbar" class="my-nav"></div>
             <!-- Author List-->
-            <div class="col-8 offset-3">
+            <div class="col-10 offset-2 ">
                 <p class="h3 mt-4">Author List</p>
                 <table class="table table-striped table-sm mt-4">
                     <tr>
                         <th>No</th>
-                        <th class="col-1">Image</th>
                         <th class="col-1">Name</th>
+                        <th class="col-1">Image</th>
                         <th>Biography</th>
-                        <th>Action</th>
+                        <th colspan="2">Action</th>
                     </tr>
-                    <?php 
+                    <?php
+                    require "../Controller/authorListController.php";
                     foreach ($result as $key => $value) {
                         echo "<tr>";
-                        echo "<td>" .$value['id']."</td>";
-                        echo "<td>" .$value['author_name']."</td>";
-                        echo "<td>" .$value['author_image']."</td>";
-                        echo "<td>" .$value['author_bio']."</td>";
-                        echo "</tr>";
+                        echo "<td>" . $value['id'] . "</td>";
+                        echo "<td>" . $value['author_name'] . "</td>";
+                        echo "<td class='aut-img'> <img src='../resource/img/authors/" . $value['author_image'] . "' </td>";
+                        echo "<td class='aut-bio w-20'>" . $value['author_bio'] . "</td>";
+                        echo "<td><a href='../Controller/updateBookController.php?id=" . $value['id'] . "'><button type='button' class='btn btn-outline-primary'><ion-icon name='create-outline'></ion-icon></button></a></td>";
+                        echo "<td><a href='../Controller/deleteBookController.php?id=" . $value['id'] . "'><button type='button' class='btn btn-outline-danger'><ion-icon name='trash-outline'></ion-icon></button></a></td>";
                     }
                     ?>
-                    
                 </table>
-                <button class="text-dec custombutton btn-sm offset-10  bg-warning  col-2"><a href="../View/addAuthor.php" class="fw-bold text-dark">ADD AUTHOR</a></button>
+                <button class="text-dec custombutton btn-sm offset-10 col-2"><a href="../View/addAuthor.php" class="fw-bold text-dark">ADD AUTHOR</a></button>
             </div>
         </div>
     </div>
-    
 </body>
-
 </html>
