bcdc048991d966ee6e02bb0cc213b09d5e6be69e
Aung Min Khant <aungmk299@gmail.com>
2022-06-03 20:45:03 +0630

    bookdetail view fix

--- 04.Development/Project/Customer/View/bookDetails.php
+++ 04.Development/Project/Customer/View/bookDetails.php
@@ -10,28 +10,29 @@
    <link rel="stylesheet" href="../resource/css/style.css" />
    <link rel="stylesheet" href="../resource/css/book_details.css" />
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
    <link rel="stylesheet" href="../resource/UI Library/fontawesome-free/css/all.min.css">
    <link rel="stylesheet" href="../resource/css/common.css">
    <link rel="stylesheet" href="../resource/css/footer.css">
-   
+
 </head>
 
 <body>
 
- <!-- Header -->
- <nav class="navbar navbar-expand-lg   top-0 w-100 " id="cum_navbar">
-      
+   <!-- Header -->
+   <nav class="navbar navbar-expand-lg   top-0 w-100 " id="cum_navbar">
+
    </nav>
    <!-- Header -->
    <section class="container-fluid">
       <div class="row align-items-center justify-content-center min-vh-100">
          <div class="col-12 col-lg-4 ps-5 ms-3">
-            <div class="slider slide-bg">
-               <div class="">
-                  <img class="img-slide1 img-fluid mt-3" src="../resource/img/book cover/bootstrap.png" alt="">
+            <div class="slider slide-bg d-flex">
+               <div class="detail">
+                  <img class="img-fluid mt-3 " src="../resource/img/book cover/bootstrap.png" alt="">
                </div>
-               <div>
-                  <img class="img-slide2 img-fluid mt-2" src="../resource/img/book cover/jsEi.png" alt="">
+               <div class="detail">
+                  <img class="img-fluid mt-2" src="../resource/img/book cover/jsEi.png" alt="">
                </div>
             </div>
          </div>
@@ -64,6 +65,23 @@
                      </div>
                   </div>
 
+                  <div class="d-flex col-md-12 col-sm-10 ">
+                     <div class="container-fluid col-md-10 col-sm-6 d-flex  mb-3">
+                        <div class="d-flex justify-content-center  col-md-5 bg-light  rounded mt-3 qty ">
+                           <span class="minus">-</span>
+                           <input type="number" class="counts" id="qty" name="qty" value="1">
+                           <span class="plus">+</span>
+                        </div>
+
+                        <div class="mt-3">
+                        
+                        <button class="btn cartbtn"><i class="fas fa-shopping-cart mx-1"></i> Add to Cart</button>
+                     </div>
+                     </div>
+
+                    
+
+                  </div>
                </div>
             </div>
          </div>
@@ -72,123 +90,82 @@
 
    <section class="container">
       <div class="row">
-         <div class="col-12 col-lg-6">
-            <div class="">
-               <div class="review-heading">
-                  <p class="btn btn-primary w-25  commom-bg">Review</p>
+         <div class="col-md-7 ">
+   
+               <div class="col-md-12">
+                  <p class="titles commom-bg">Review</p>
                </div>
-               <div class="review-box">
-               </div>
-               <div class="commentboxpossition">
+               <div class="col-md-12">
                   <div class="commentbox">
                      <div class="commentboxheader">
-                        <h2 class="QuestionHeader">Review About This Product</h2>
+                        <p class="QuestionHeader">Review About This Product</p>
                      </div>
                      <div class="commentbody">
-                        <div class="userComment" id="commentbox1">
-                           <div class="commentinner" id="commentoutput1"></div>
-                        </div>
-                        <div class="userComment" id="commentbox2">
-                           <div class="commentinner" id="commentoutput2"></div>
-                        </div>
-                        <div class="userComment" id="commentbox3">
-                           <div class="commentinner" id="commentoutput3"></div>
-                        </div>
-                        <div class="userComment" id="commentbox4">
-                           <div class="commentinner" id="commentoutput4"></div>
-                        </div>
-                        <div class="userComment" id="commentbox5">
-                           <div class="commentinner" id="commentoutput5"></div>
-                        </div>
+                       
                      </div>
-                  </div>
-                  <div class="commentinput">
-                     <input type="text" class="inputtextbox" id="inputtextbox" />
-                     <button class="sendbutton btn btn-primary" onclick="Sendcomment()">
+                       <div class="commentinput mt-4">
+                      
+                     <input type="text" class="inputtextbox" id="inputtextbox"  placeholder="Please Type here..."/>
+                     <button class="sendbutton">
                         Post Your Review
                      </button>
                   </div>
+                       
+                  </div>
+                
+                
                </div>
             </div>
 
+            <div class="col-md-4">
+            <div class="col-md-12">
+                  <p class="titles commom-bg">Description</p>
+               </div>
+               <div class="col-md-12">
+                  <div class="descbox">
+                     <div class="desctable ">
+                        <table class="table table-bordered m-5">
+                           <tr>
+                              <td >Pages</td>
+                              <td>200</td>
+                           </tr>
+                           <tr>
+                              <td >Release Date</td>
+                              <td>2008</td>
+                           </tr>
+                           <tr>
+                              <td>Size of Book</td>
+                              <td>5.5*0.5*6.2 inches</td>
+                           </tr>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
          <div class="col-12 col-lg-1"></div>
          <div class="col-12 col-lg-5"></div>
       </div>
    </section>
    <footer id="footer" class="container-fluid commom-bg  mt-5">
-     
+
    </footer>
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    <script src="../resource/js/common.js"></script>
    <script src="../resource/js/footerCommon.js"></script>
-   
+  
    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
    <script src="../resource/js/homepage.js"></script>
    <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
-   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+   <!-- <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script> -->
+   <!-- <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> -->
    <script src="../resource/UI Library/fontawesome-free/js/all.min.js"></script>
-   
+   <script src="../resource//js/bookDetails.js"></script>
    <script type="text/javascript">
       $(".slider").slick({
-         prevArrow: '<span class="priv_arrow btn  btn-primary "><i class="fas fa-angle-left"></i></span>',
-         nextArrow: '<span class="next_arrow btn btn-primary "><i class="fas fa-angle-right"></i></span>'
+         prevArrow: '<span class="slick-prev slick-arrow d-flex align-items-center m-5  arrows"><i class="fas fa-angle-left"></i></span>',
+         nextArrow: '<span class="slick-next slick-arrow d-flex align-items-center m-5 arrows"><i class="fas fa-angle-right"></i></span>'
       });
-
-      function Sendcomment() {
-         var input = document.getElementById("inputtextbox").value;
-         var output1 = document.getElementById("commentoutput1").innerHTML;
-         var output2 = document.getElementById("commentoutput2").innerHTML;
-         var output3 = document.getElementById("commentoutput3").innerHTML;
-         var output4 = document.getElementById("commentoutput4").innerHTML;
-         var output5 = document.getElementById("commentoutput5").innerHTML;
-         if (output1 == "") {
-            document.getElementById("commentbox1").style.display = "block"
-            document.getElementById("commentoutput1").innerHTML = input
-
-         } else if (output2 == "") {
-            document.getElementById("commentbox2").style.display = "block"
-            document.getElementById("commentoutput2").innerHTML = input
-         } else if (output3 == "") {
-            document.getElementById("commentbox3").style.display = "block"
-            document.getElementById("commentoutput3").innerHTML = input
-         } else if (output4 == "") {
-            document.getElementById("commentbox4").style.display = "block"
-            document.getElementById("commentoutput4").innerHTML = input
-         } else if (output5 == "") {
-            document.getElementById("commentbox5").style.display = "block"
-            document.getElementById("commentoutput5").innerHTML = input
-         }
-
-
-         document.getElementById("inputtextbox").value = "";
-      }
-
-      function CommentDelete1() {
-         document.getElementById("commentbox1").style.display = "none"
-         document.getElementById("commentoutput1").innerHTML = ""
-      }
-
-      function CommentDelete2() {
-         document.getElementById("commentbox2").style.display = "none"
-         document.getElementById("commentoutput2").innerHTML = ""
-      }
-
-      function CommentDelete3() {
-         document.getElementById("commentbox3").style.display = "none"
-         document.getElementById("commentoutput3").innerHTML = ""
-      }
-
-      function CommentDelete4() {
-         document.getElementById("commentbox4").style.display = "none"
-         document.getElementById("commentoutput4").innerHTML = ""
-      }
-
-      function CommentDelete5() {
-         document.getElementById("commentbox5").style.display = "none"
-         document.getElementById("commentoutput5").innerHTML = ""
-      }
    </script>
 </body>
 
