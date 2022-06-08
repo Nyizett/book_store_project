d3e95bfacb420f862aba91ea35a8198b87adf326
Aung Min Khant <aungmk299@gmail.com>
2022-06-08 19:55:10 +0630

    book list book detail

---
+++ 04.Development/Project/Customer/Controller/bookDetailController.php
@@ -0,0 +1,23 @@
+<?php
+
+if(isset($_GET['id'])) {
+    
+        require "../../Admin/Model/DBConnection.php";   
+        $db =  new DBConnect();
+        $dbconnect = $db->connect();
+        $id= $_GET['id'];
+
+        $sql = $dbconnect->prepare("SELECT * FROM `m_book` INNER JOIN `m_author` ON m_book.author_id = m_author.id WHERE m_book.id = $id AND m_book.del_flg =0 ");
+        $sql->execute();
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+        if(count($result) > 0){
+            require '../../Customer/View/bookDetails.php';
+        }else{
+            echo "ur book does not exist";
+        }
+      
+}else{
+    echo 'no';
+}
+
+
--- 04.Development/Project/Customer/View/bookDetails.php
+++ 04.Development/Project/Customer/View/bookDetails.php
@@ -21,19 +21,100 @@
 
    <!-- Header -->
    <nav class="navbar navbar-expand-lg   top-0 w-100 " id="cum_navbar">
+   <div class="container-fluid">
+  <div class="image-container">
+    <img src="../resource/img/logo.png" alt="main-logo" id="logo" class="main-logo" >
+   </div>
+    <a class="my-title navbar-brand font-color-primary pt-4" href="#">
+        <span class="my-fcolor fw-bolder fs-4 border-bottom">စာ အုပ် ဆိုင်</span>
+        <p class="b-title">Book Store</p>
+    </a>
+  <button
+    class="navbar-toggler font-color-primary"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <ion-icon
+      class="my-fcolor font-color-primary fs-2"
+      name="menu-outline"
+    ></ion-icon>
+  </button>
+  <button class="categories border-white dropdown btn commom-bg ms-4">
+    <a
+      class="text-decoration-none fs-6 dropdown-toggle text-white"
+      href="#"
+      id="navbarDropdown"
+      role="button"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+    >
+      <ion-icon class="pe-2 pt-2" name="apps-outline"></ion-icon>
+      <span class="my-fcolor cat d-inlineblock text-center">Categories </span>
+   
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+      <li><a class="dropdown-item" href="#">Language</a></li>
+      <li><a class="dropdown-item" href="#">Education</a></li>
+      <li><a class="dropdown-item" href="#">Novels</a></li>
+      <li><a class="dropdown-item" href="#">Technology</a></li>
+      <li><a class="dropdown-item" href="#">Cartoons</a></li>
+    </ul>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ms-auto me-4 mb-2 mb-lg-0">
+      <li class="nav-item me-4">
+        <a class="text-white nav-link text-decoration-none fw-bold fs-6 border-bottom border-white" aria-current="page"
+          href="#">Home</a>
+      </li>
+      <li class="nav-item me-4">
+        <a class="text-white nav-link font-color-primary text-decoration-none fw-bold fs-6" href="#">Search</a>
+      </li>
+      <li class="nav-item me-4">
+        <a class="text-white nav-link text-decoration-none fw-bold fs-6" href="#" tabindex="-1">Authors</a>
+      </li>
+      <li class="nav-item me-4">
+        <a class="text-white nav-link text-decoration-none fw-bold fs-6" href="#" tabindex="-1">Shops</a>
+      </li>
 
+      <li class="nav-item dropdown me-4">
+        <a class="text-white nav-link text-decoration-none fw-bold fs-6 dropdown-toggle" href="#" id="navbarDropdown"
+          role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Contact us
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="my-fcolor dropdown-item" href="#">View Profile</a></li>
+          <li><a class="my-fcolor dropdown-item" href="#">Guide</a></li>
+          <li><a class="my-fcolor dropdown-item" href="#">Servies</a></li>
+          <li>
+            <a class="my-fcolor dropdown-item" href="#">Privacy and Policy</a>
+          </li>
+          <li><a class="my-fcolor dropdown-item" href="#">FAQ's</a></li>
+        </ul>
+      </li>
+    </ul>
+    <div class="d-flex justify-content-center align-items-center font-color-primary">
+      <ion-icon class="text-white fs-4 me-4" name="cart-outline"></ion-icon>
+      <ion-icon class="text-white fs-4 me-4 person-hover" name="person-outline"></ion-icon>
+      <ion-icon class="text-white fs-4" name="moon-outline"></ion-icon>
+    </div>
+  </div>
+</div>
    </nav>
    <!-- Header -->
+   
    <section class="container-fluid">
       <div class="row align-items-center justify-content-center min-vh-100">
          <div class="col-12 col-lg-4 ps-5 ms-3">
             <div class="slider slide-bg d-flex">
                <div class="detail">
-                  <img class="img-fluid mt-3 " src="../resource/img/book cover/bootstrap.png" alt="">
+                  <?php 
+                  echo "<img class='img-fluid mt-3' src='../resource/img/book cover/".$result[0]['book_image']."' alt=''>"
+               ?>                 
                </div>
-               <div class="detail">
-                  <img class="img-fluid mt-2" src="../resource/img/book cover/jsEi.png" alt="">
-               </div>
+               
             </div>
          </div>
 
@@ -44,7 +125,7 @@
             <div class="">
                <div class="card mb-5 mb-lg-0 card-width ">
                   <div class="card-body d-flex flex-column ">
-                     <p class="card-title fw-bold mb-0 font-color-primary">JavaScript</p>
+                     <p class="card-title fw-bold mb-0 font-color-primary"></p>
                      <small>
                         <i class="fa-solid fa-star text-warning"></i>
                         <i class="fa-solid fa-star text-warning"></i>
@@ -52,16 +133,16 @@
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                      </small>
-                     <small class="card-text d-block mb-3 font-color-secondary">
-                        Ei Maung
+                    
+                        <?php echo " <small class='card-text d-block mb-3 font-color-secondary'>". $result[0]['author_name']." </small>";
+                        ?>
                      </small>
                      <div class="d-flex justify-content-between align-items-center">
-                        <p class="fw-bold mb-0 font-color-primary">3000 MMK</p>
+                       <?php echo " <p class='fw-bold mb-0 font-color-primary'>". number_format($result[0]['book_price'])." MMK</p>"; ?>
                      </div>
 
                      <div class="details-text  mt-2">
-                        <p class="p-3"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate delectus, tenetur earum esse at similique corrupti sapiente dolor maiores distinctio eos, laborum rem accusamus quam consectetur! Cum, autem nobis!
-                        </p>
+                       <?php echo " <p class='p-3'>".$result[0]['book_description']."</p>" ?>
                      </div>
                   </div>
 
@@ -127,15 +208,15 @@
                         <table class="table table-bordered m-5">
                            <tr>
                               <td >Pages</td>
-                              <td>200</td>
+                              <?php echo "<td>".$result[0]['book_pages']."</td>" ?>
                            </tr>
                            <tr>
                               <td >Release Date</td>
-                              <td>2008</td>
+                              <?php echo "<td>".$result[0]['book_established']."</td>" ?>
                            </tr>
                            <tr>
                               <td>Size of Book</td>
-                              <td>5.5*0.5*6.2 inches</td>
+                              <?php echo "<td>".$result[0]['book_size']."</td>" ?>
                            </tr>
                         </table>
                      </div>
@@ -148,7 +229,112 @@
       </div>
    </section>
    <footer id="footer" class="container-fluid commom-bg  mt-5">
-
+   <div class="row">
+    <div class="col-12 col-lg-3 ps-5 pt-4">
+        <div class="">
+            <h2 class="text-white align-top">စာ အုပ် ဆိုင်</h2>
+            <p></p>
+            <p class="text-white">Address</p>
+            <p class="text-white">SHOW ON MAP</p>
+            <div class="d-flex">
+                <i class="fab fa-facebook text-white mx-1"></i>
+                <i class="fab fa-instagram text-white mx-1"></i>
+                <i class="fab fa-youtube text-white mx-1"></i>
+                <i class="fab fa-twitter text-white mx-1"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-8 col-xl-6">
+        <div class="container-fluid">
+            <div class="row mt-5 hello">
+                <div class="col-6 col-lg-3 text-white">
+                    <div class="cs-width me-5">
+                        <p class="fw-bold">Customer Service</p>
+                        <small>09-123456789</small>
+                        <br />
+                        <small>hello@gmail.com</small>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 text-white">
+                    <div class="">
+                        <p class="fw-bold">Our Service</p>
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Contact Us </small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small> Guide </small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Delivery Free</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small> Good Print Quality</small>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                    <div class="">
+                        <p class="fw-bold text-white">Categories</p>
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Language</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Education</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Novels</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Technology</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Cartoons</small>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                    <div class="">
+                        <p class="fw-bold text-white">Shop List</p>
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Yagnon</small>
+                        </a>
+                        <br />
+                        <a href="#" class="btn-link text-white text-decoration-none">
+                            <small>Mandalay </small>
+                        </a>
+                        <br />
+                        <a href="tel:09250152018" class="btn-link text-white text-decoration-none">
+                            <small>NayPyiTaw</small>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <hr class="text-white" />
+        <div class="col-12 col-lg-6">
+            <div class="">
+                <p class="text-white">
+                    Copyright © 2022 ParadiseBookStore All rights reserved.
+                </p>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6">
+            <div class="d-flex justify-content-end">
+                <i class="fab fa-cc-visa text-white mx-1"></i>
+                <i class="fab fa-cc-mastercard text-white mx-1"></i>
+            </div>
+        </div>
+    </div>
+</div>
    </footer>
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    <script src="../resource/js/common.js"></script>
