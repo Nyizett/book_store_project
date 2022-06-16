cd44b05cda504a5148f41c9a30347e94eb029c22
Merries <merriesmmk5814@gmail.com>
2022-06-16 22:45:32 +0630

    no message

--- 04.Development/Project/Customer/View/search.php
+++ 04.Development/Project/Customer/View/search.php
@@ -25,8 +25,8 @@
    <!-- Header -->
 
    <div class="resp header container-fluid d-flex justify-content-between align-items-center p-4">
-      <h3 class="ms-5 text-primary">Search</h3>
-      <div class="me-5 text-primary">
+      <h3 class="search ms-5 ">Search</h3>
+      <div class="hometextColor me-5 ">
          <b>Home</b> -> Search
       </div>
    </div>
