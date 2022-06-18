b80bd09115aa00b72a7f5938e32335910d7801ac
Merries <merriesmmk5814@gmail.com>
2022-06-18 12:36:14 +0630

    no message

--- 04.Development/Project/Customer/Controller/changeFaviconController.php
+++ 04.Development/Project/Customer/Controller/changeFaviconController.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once "../../Admin/Model/DBConnection.php";
+require "../../Admin/Model/DBConnection.php";
 
 
 
 //Call DB Connection
 $dbfav =  new DBConnect();
-$dbconnectfav = $dbfav ->connect();
+$dbconnectfav = $dbfav->connect();
 
 $sql = $dbconnectfav->prepare(
     "SELECT fav_icon FROM m_site_master
