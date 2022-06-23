4dc42a10c5844b6323b329a85672c569d7ca60b6
DESKTOP-KK6PVSK\user <nyimyomwa9@gmail.com>
2022-06-23 20:32:58 +0630

    no message

--- 04.Development/Project/Admin/Controller/updateDBBookController.php
+++ 04.Development/Project/Admin/Controller/updateDBBookController.php
@@ -19,11 +19,7 @@ if(isset($_POST)){
 
     $file = $_FILES['bookFile']['name'];
     $location = $_FILES['bookFile']['tmp_name'];
-<<<<<<< HEAD
-    // $image = file_get_contents($location);
-=======
     
->>>>>>> origin/main
 
     // echo $file;
 
--- 04.Development/Project/Admin/Controller/updateDBShopController.php
+++ 04.Development/Project/Admin/Controller/updateDBShopController.php
@@ -43,28 +43,28 @@ if (isset($_POST)) {
             $sql->bindValue(":adminName", $UserName);
             $sql->bindValue(":id", $id);
             $sql->execute();
-        } else {
-            $sql = $dbconnect->prepare(
-                "UPDATE m_shop SET
-                shop_city = :city,
-                shop_name = :name,
-                shop_address= :address,
-                shop_phone_no = :phone,
-                shop_website = :web,
-                update_date=:todayDate,
-                update_by=:adminName
-                WHERE id = :id"
-            );
-            $sql->bindValue(":city", $shopCity);
-            $sql->bindValue(":name", $shopName);
-            $sql->bindValue(":address", $shopAddress);
-            $sql->bindValue(":phone", $shopPhone);
-            $sql->bindValue(":web", $shopWebsite);
-            $sql->bindValue(":todayDate", date("d/m/Y"));
-            $sql->bindValue(":adminName", $UserName);
-            $sql->bindValue(":id", $id);
-            $sql->execute();
-        }
+        } 
+    }else {
+        $sql = $dbconnect->prepare(
+            "UPDATE m_shop SET
+            shop_city = :city,
+            shop_name = :name,
+            shop_address= :address,
+            shop_phone_no = :phone,
+            shop_website = :web,
+            update_date=:todayDate,
+            update_by=:adminName
+            WHERE id = :id"
+        );
+        $sql->bindValue(":city", $shopCity);
+        $sql->bindValue(":name", $shopName);
+        $sql->bindValue(":address", $shopAddress);
+        $sql->bindValue(":phone", $shopPhone);
+        $sql->bindValue(":web", $shopWebsite);
+        $sql->bindValue(":todayDate", date("d/m/Y"));
+        $sql->bindValue(":adminName", $UserName);
+        $sql->bindValue(":id", $id);
+        $sql->execute();
     }
     header ("Location: ../View/shopInfo.php");
 }
